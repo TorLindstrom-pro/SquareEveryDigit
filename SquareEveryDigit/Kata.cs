61067fdb5db2a722cc6dd778bca5a6ba3e324bbd
Tor Lindström <tor.lindstrom.jobb@gmail.com>
2024-10-12 08:54:53 +0200

    feat: each digit is squared

--- SquareEveryDigit/Kata.cs
+++ SquareEveryDigit/Kata.cs
@@ -1,9 +1,12 @@
+using System.Linq;
+
 namespace SquareEveryDigit;
 
 public class Kata
 {
     public static int SquareDigits(int n)
     {
-        return n*n;
+        var squareDigits = n.ToString().Select(char.GetNumericValue).Aggregate("", (result, c) => result + c * c);
+        return int.Parse(squareDigits);
     }
 }
--- Test/SquareDigitsTests.cs
+++ Test/SquareDigitsTests.cs
@@ -14,12 +14,22 @@ public class SquareDigitsTests
     [TestCase(1, 1)]
     [TestCase(4, 4)]
     [TestCase(81, 81)]
-    public void ZeroSquared_IsZero(int n, int expectedResult)
+    public void SingleDigits_AreSquared(int n, int expectedResult)
     {
         // Act
         var result = Kata.SquareDigits(0);
 
         // Assert
         Assert.That(result, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void EachDigitInNumber_IsSquared()
+    {
+        // Act
+        var result = Kata.SquareDigits(123);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(149));
     }
 }
